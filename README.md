1bc0d17b10786a88c09fa2d5567359068f0796d6
RenanPeres <renanperes99@usp.br>
2024-06-23 12:27:23 -0300

    final project scripts

--- README.md
+++ README.md
@@ -4,8 +4,9 @@
 This repository contains a collection of projects, assignments, and coursework completed during my time at university. It includes work from various subjects and disciplines, showcasing my academic growth and practical skills. The repository is organized by semester and course, providing a comprehensive overview of my academic journey.
 
 ## Content
-- Introduction to Computer Science (Sparse matrix)
-- Data Structures III (Database manager and file system simulator)
+- Bachelor's final project (SSC0670_final_project)
+- Introduction to Computer Science -> Sparse matrix project
+- Data Structures III -> Database manager and file system simulator
 - SSC0661 -> Computer Network
 - SSC0620 -> Software Engineering
 - SSC0740 -> Operations System I
---
+++ SSC0670_final_project/ep3_non_context.py
@@ -0,0 +1,59 @@
+import ktrain
+from sklearn import metrics
+import matplotlib.pyplot as plt
+import pandas as pd
+from sklearn.metrics import classification_report
+
+NAME_TERMINAL = "ep3_non_context"
+
+
+#Importando a biblioteca pandas e carregando as amostras
+df_test = pd.read_pickle('Amostras_Teste.pkl')
+print(df_test.shape)
+df_test = df_test.dropna()
+print(df_test.shape)
+print("importou as bibliotecas")
+
+#Load predictor
+p = ktrain.load_predictor('bert-base-multilingual-cased_e3_apps.model')
+print("carregou o modelo")
+
+# teste com contexto
+x_test = df_test.review.values
+x_test = list(map(tuple, x_test))
+y_true = [df_test.label_feelings.values, df_test.negative_index.values]
+
+#Amostras gerais
+print(NAME_TERMINAL)
+
+#Predições
+y_pred = p.predict(x_test)
+print("terminado as predições")
+y_pred_proba = p.predict_proba(x_test)
+print("terminado as probabilidades de predições")
+
+#Exibindo as métricas obtidas
+print(classification_report(y_true[0][0:len(y_pred)], y_pred))
+
+#Calculando a curva ROC e AUC
+fpr, tpr, _ = metrics.roc_curve(y_true[1],  y_pred_proba[:, 0])
+auc = metrics.roc_auc_score(y_true[1],  y_pred_proba[:, 0])
+
+#create ROC curve
+plt.figure()
+lw = 2
+plt.plot(
+    fpr,
+    tpr,
+    color="darkorange",
+    lw=lw,
+    label="ROC curve (area = %0.4f)" % auc,
+)
+plt.xlabel("False Positive Rate")
+plt.ylabel("True Positive Rate")
+plt.legend(loc="lower right")
+plt.savefig("./00 - imagens/"+NAME_TERMINAL+".png", 
+            dpi=300,
+            transparent=True)
+
+print("Fim da execução de "+NAME_TERMINAL)
